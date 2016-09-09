fd35e2d0b923ca878395ed58781bb0edfa4cda69
sceulemans <sceulemans@7662SY1.ctg.com>
2016-09-09 15:12:47 +0200

    NewTest Probeersel

--- src/test/ApplicationTest.java
+++ src/test/ApplicationTest.java
@@ -2,8 +2,9 @@ package test;
 
 import junit.framework.TestCase;
 
-public class Test extends TestCase  {
+public class ApplicationTest extends TestCase  {
 	
+
 	 public void testAdd(){
 	      assertTrue(3*2 == 6);
 	   }
